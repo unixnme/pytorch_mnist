f3566eaef22090621bf99b93a001c016402a1a10
Unix&Me <unixnme@gmail.com>
2018-04-26 22:24:21 +0900

    add bn

--- mnist.py
+++ mnist.py
@@ -57,15 +57,23 @@ class Net(nn.Module):
     def __init__(self, ndim=2):
         super(Net, self).__init__()
         self.ndim = ndim
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=3)
+        self.bn1 = nn.BatchNorm2d(10, affine=True)
+        self.conv2 = nn.Conv2d(10, 10, kernel_size=3)
+        self.conv3 = nn.Conv2d(10, 20, kernel_size=3)
+        self.bn2 = nn.BatchNorm2d(20, affine=True)
+        self.conv4 = nn.Conv2d(20, 20, kernel_size=3)
+        self.conv4_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(320, ndim)
         self.softmax = AngleSoftmax(10)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = F.relu(self.conv1(x))
+        x = self.bn1(x)
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(self.conv3(x))
+        x = self.bn2(x)
+        x = F.relu(F.max_pool2d(self.conv4_drop(self.conv4(x)), 2))
         x = x.view(-1, 320)
         x = self.fc1(x)
         x = self.softmax(x)
@@ -77,8 +85,12 @@ class FeatureExtractor(Net):
         self.load_state_dict(model.state_dict())
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = F.relu(self.conv1(x))
+        x = self.bn1(x)
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(self.conv3(x))
+        x = self.bn2(x)
+        x = F.relu(F.max_pool2d(self.conv4_drop(self.conv4(x)), 2))
         x = x.view(-1, 320)
         x = self.fc1(x)
         return x
